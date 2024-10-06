187b0df31c14d427117ad6f041637858ff855b0f
RahulAnand-GitHub <rahul.anand88096@gmail.com>
2024-10-06 16:28:43 +0530

    Handled multiple routes with different syntax.

--- src/app.js
+++ src/app.js
@@ -2,9 +2,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get(/.*a$/, (req, res) => {
-  res.send("Hello from the server.");
-});
+app.get(
+  "/",[
+  (req, res, next) => {
+    console.log("1st Response");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("Hello from the server2.");
+    console.log("2nd Response!");
+    next();
+  }],
+  (req, res, next) => {
+    console.log("3rd Response!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("4th Response!");
+    res.send('Response form server 4.')
+  }
+);
 
 app.listen(port, () => {
   console.log("Server of successfully listening port 3000.");
