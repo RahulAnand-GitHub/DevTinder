6c0af26aced1a21ce2d87f520fc7faf73db8278b
RahulAnand-GitHub <rahul.anand88096@gmail.com>
2024-10-08 23:57:54 +0530

    Database introduced.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,15 @@
 const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
 const port = 3000;
-const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.use("/admin", adminAuth);
-// app.use("/user", userAuth);
-
-app.get("/user", userAuth, (req, res) => {
-  res.send("User Data");
-});
-
-app.get("/admin/getAllDAta", adminAuth => {
-  res.send("All admin data");
-});
-
-app.get("/admin/deleteUser", adminAuth => {
-  res.send("Deleted use data");
-});
-
-app.listen(port, () => {
-  console.log("Server of successfully listening port 3000.");
-});
+connectDB()
+  .then(() => {
+    console.log("Database connection is successful.");
+    app.listen(port, () => {
+      console.log("Server of successfully listening port 3000.");
+    });
+  })
+  .catch((err) => {
+    console.error(`${err} : Error in connection.`);
+  });
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://rahulanand88096:Mongo~~~72@namastenode.6yfgq.mongodb.net/devTinder"
+  );
+};
+
+module.exports = connectDB;
