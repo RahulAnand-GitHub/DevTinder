a5e406dd642140687e1e2b0ab031092510c9aacd
RahulAnand-GitHub <rahul.anand88096@gmail.com>
2024-11-18 11:57:22 +0530

    Applied data sanitization and validations

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const User = require('./models/user')
 const app = express()
 const port = 3000
 app.use(express.json())
+
 app.post('/signup', async (req, res) => {
   const user = new User(req.body)
   try {
@@ -15,6 +16,49 @@ app.post('/signup', async (req, res) => {
 
   //Reading the request
   console.log(req.body)
+})
+//geting data from the database
+app.get('/user', async (req, res) => {
+  const userEmail = req.body.email
+
+  try {
+    const user = await User.find({ email: userEmail })
+    if (user.length === 0) {
+      res.status(404).send('User not found.')
+    } else {
+      res.send(user)
+    }
+  } catch (e) {
+    res.status(400).send('Something went wrong')
+  }
+})
+
+//deleting data from the database
+app.delete('/user', async (req, res) => {
+  const userId = req.body.userId
+  try{
+    const user = await User.findByIdAndDelete(userId);
+    res.send('User deleted successfully.')
+  }catch(e){
+    res.status(400).send('Something went wrong.')
+  }
+})
+
+//updating data in the database
+app.patch('/user', async(req, res)=>{
+  const userId = req.body.userId
+  const data = req.body
+  try{
+    const user = await User.findByIdAndUpdate(userId, data,{
+      returnDocument:'after',
+      runValidators: true,
+    })
+    console.log(user)
+    res.send('User updated successfully.')
+
+  }catch(e){
+    res.status(400).send('Something went wrong.')
+  }
 })
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,55 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min:18,
+    },
+    gender: {
+      type: String,
+      validate(value){
+        if(!['M', 'F', 'Others'].includes(value)){
+          throw new Error('Gender data is not valid!');
+          
+        }
+      }
+    },
+    photoUrl: {
+      type: String,
+      defalut:
+        'https://www.msrcasc.edu.in/uploads/media-upload/2023-03/hod-dummy.jpg',
+    },
+    about: {
+      type: String,
+      default: 'This is the default description about the user!',
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-})
+  {
+    timestamps: true,
+  }
+)
 
 const User = mongoose.model('User', userSchema)
 module.exports = User
