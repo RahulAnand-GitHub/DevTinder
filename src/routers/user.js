23b0077e2610ea59601303fb0e416484554e07f6
RahulAnand-GitHub <rahul.anand88096@gmail.com>
2025-01-31 00:57:20 +0530

    Pagination added.

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router()
 
 const { userAuth } = require('../middlewares/auth')
 const ConnectionRequest = require('../models/connectionRequest')
+const User = require('../models/user')
 
 userRouter.get('/user/requests/received', userAuth, async (req, res) => {
   try {
@@ -61,4 +62,37 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
   }
 })
 
+userRouter.get('/user/feed', userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user
+
+    const page = parseInt(req.query.page) || 1
+    let limit = parseInt(req.query.limit) || 10
+    limit = limit > 2 ? 2  : limit
+    const skip = (page - 1) * limit
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select(['fromUserId', 'toUserId'])
+
+    const hiddednUserFromFeed = new Set()
+    connectionRequests.forEach((req) => {
+      hiddednUserFromFeed.add(req.fromUserId.toString())
+      hiddednUserFromFeed.add(req.toUserId.toString())
+    })
+    // console.log(hiddednUserFromFeed)
+
+    const user = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hiddednUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(['firstName', 'lastName', 'age', 'skills', 'about'])
+      .skip(skip)
+      .limit(limit)
+    res.send(user)
+  } catch (e) {
+    res.status(400).json({ message: e.message })
+  }
+})
 module.exports = userRouter
